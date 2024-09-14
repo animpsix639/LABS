0a3767913ab4949a4afa06745348c9222986184a
animpsix639 <khleborezka@mail.ru>
2024-09-14 16:54:56 +0300

    добавил лабораторную 1

---
+++ lab1/task_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // 1
+using namespace std; // 2
+
+int main() // 3
+{
+
+    cout << "First string" << endl; // 4
+    cout << "Первая строка\nВторая строка" << endl; // 5
+    cout << "Спецсимволы \"\\\"" << endl; // 123
+
+} 
---
+++ lab1/task_2.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a = 150;
+    float b = 15.933;
+    unsigned char c = 250;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << static_cast<int>(c) << endl;
+
+    int day = 31;
+    string month = "Май";
+    int year = 2005;
+
+    cout << "Моя дата рождения: " << day << ' ' << month << ' ' << year << " года" << endl;
+
+    const float number = 2.3;
+    const string os = "WINDOWS";
+
+    cout << number << ' ' << os << endl;
+    return 0;
+}
